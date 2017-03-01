6330fe00bc89f5701a3018218ff600e94ae7a6f8
Gavin Nitta <gshigeto@gmail.com>
2017-03-01 12:48:15 -0700

    Fix canvas background image

--- app/scripts/home.directive.js
+++ app/scripts/home.directive.js
@@ -14,6 +14,29 @@ angular.module('Creative3.home', ['pw.canvas-painter', 'color.picker'])
           $scope.version = $scope.version - 1;
         };
 
+        $scope.download = function () {
+          var h = document.getElementById('pwCanvasMain');
+          window.open(h.toDataURL())
+        }
+
+        $scope.image = function () {
+          var canvas = document.getElementById("pwCanvasMain");
+          var ctx = canvas.getContext("2d");
+
+          if ($scope.imageSrc === '') {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            canvas.style.background = '#FFF';
+          } else {
+            var background = new Image();
+            background.src = $scope.imageSrc;
+            background.crossOrigin = 'anonymous';
+
+            background.onload = function () {
+              ctx.drawImage(background, 0, 0);
+            };
+          }
+        };
+
       }
     };
   }]);
