e5723275aabcf5e93f2e589f3d283d6a19baba86
Gavin Nitta <gshigeto@gmail.com>
2017-03-01 12:14:49 -0700

    Add drawing area to home page

--- app/scripts/home.directive.js
+++ app/scripts/home.directive.js
@@ -1,13 +1,21 @@
 'use strict';
 
-angular.module('Creative3.home', [])
+angular.module('Creative3.home', ['pw.canvas-painter', 'color.picker'])
   .directive('homePage', [function () {
     return {
       restrict: 'E',
       link: function ($scope) {
-        $scope.homeMessage = 'This is the home directive';
+        $scope.imageSrc = '';
+        $scope.version = 0;
+        $scope.lineWidth = 1;
+        $scope.selectedColor = '#FFA500';
+
+        $scope.undo = function () {
+          $scope.version = $scope.version - 1;
+        };
+
       }
-    }
+    };
   }]);
 
 angular.module('Creative3').requires.push('Creative3.home');
